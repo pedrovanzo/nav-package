c9ca9ba9053de0deea9be1dc8ac140fc597fdf9a
pedrovanzo <pedro_vanzo@hotmail.com>
2023-06-18 17:18:43 -0300

    updates webpack 2

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 function navComponent() {
+    /* #################################### */
+    /* Creating Elements */
+    /* #################################### */
     // Navbar
     const newNavComponent = document.createElement('nav')
-    // newNavComponent.className('pv-nav')
     newNavComponent.setAttribute('class', 'pv-nav')
     // Navbar Heading
     const newNavHeading = document.createElement('h1')
-    // newNavHeading.className('pv-heading')
     newNavHeading.setAttribute('class', 'pv-heading')
+    newNavHeading.innerText = 'Main'
     // Navbar UL
     const newNavUl = document.createElement('ul')
     newNavUl.setAttribute('class', 'pv-ul')
-    // Navbar LI
-    const newNavLi = document.createElement('li')
-    newNavLi.setAttribute('class', 'pv-li')
-
-    /* APPEND LOGIC */
-
+    // Navbar LI One
+    const newNavLiOne = document.createElement('li')
+    newNavLiOne.innerText = 'navli one'
+    newNavLiOne.setAttribute('class', 'pv-li')
+    // Navbar LI Two
+    const newNavLiTwo = document.createElement('li')
+    newNavLiTwo.innerText = 'navli two'
+    newNavLiTwo.setAttribute('class', 'pv-li')
+    /* #################################### */
+    /* Appending Elements to DOM */
+    /* #################################### */
     // Append Li to Ul
-    newNavUl.appendChild(newNavLi, newNavLi, newNavLi)
+    newNavUl.appendChild(newNavLiOne)
+    newNavUl.appendChild(newNavLiTwo)
     // Append Heading to Navbar
     newNavComponent.appendChild(newNavHeading)
     // Append Ul to Navbar
